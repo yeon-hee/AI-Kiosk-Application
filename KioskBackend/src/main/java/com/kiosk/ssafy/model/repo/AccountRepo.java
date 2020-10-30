de1e315b000f131cb61f5a0e4627d79a7712e6fa
rkdals213 <rkdals213@naver.com>
2020-10-30 14:21:03 +0900

    getAccountInfo 추가 이름으로 직원 리스트 검색

--- KioskBackend/src/main/java/com/kiosk/ssafy/model/repo/AccountRepo.java
+++ KioskBackend/src/main/java/com/kiosk/ssafy/model/repo/AccountRepo.java
@@ -1,15 +1,19 @@
 package com.kiosk.ssafy.model.repo;
 
 import com.kiosk.ssafy.model.dto.Account;
+import com.kiosk.ssafy.model.dto.inter.AccountInfo;
 import com.kiosk.ssafy.model.dto.inter.AccountName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AccountRepo extends JpaRepository<Account, Long> {
 
     Optional<AccountName> findNameByEmail(String id);
+
+    List<AccountInfo> findAccountInfoByName(String name);
 
 }
--- KioskBackend/src/main/java/com/kiosk/ssafy/model/service/AccountService.java
+++ KioskBackend/src/main/java/com/kiosk/ssafy/model/service/AccountService.java
@@ -1,9 +1,12 @@
 package com.kiosk.ssafy.model.service;
 
+import com.kiosk.ssafy.model.dto.inter.AccountInfo;
 import com.kiosk.ssafy.model.dto.inter.AccountName;
 import com.kiosk.ssafy.model.repo.AccountRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AccountService {
@@ -11,7 +14,15 @@ public class AccountService {
     @Autowired
     AccountRepo accountRepo;
 
-    public AccountName findNameById(String id){
+    public AccountName findNameById(String id) {
         return accountRepo.findNameByEmail(id).get();
+    }
+
+    public List<AccountInfo> findAccountInfo(List<String> nameList) {
+        for (String name : nameList) {
+            List<AccountInfo> temp = accountRepo.findAccountInfoByName(name);
+            if(temp.size() != 0) return temp;
+        }
+        return null;
     }
 }
