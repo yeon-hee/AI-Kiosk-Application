e2efb6e1e0803b257cb756715629ef59f450535d
tiroring09 <tiroring09@gmail.com>
2020-11-13 10:52:44 +0900

    Update 홈페이지 스타일 개선
    In progress/S03P31A508-71

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -5,9 +5,10 @@
             <div class="container d-flex h-100">
             <div class="row justify-content-center align-self-center" data-aos="fade-up">
                 <div class="col-md-6 intro-info order-md-first order-last" data-aos="zoom-in" data-aos-delay="100">
-                <h2>AI Solutions<br>for Your <span>Business!</span></h2>
+                <h2>WHO <span>A</span>M <span>I</span></h2>
+                <h3>AI기반 비접촉 출입관리 솔루션</h3>
                 <div>
-                    <button @click="login" href="#services" v-if="!$store.state.isSigned" class="btn-get-started scrollto">Get Started</button>
+                    <button @click="login" v-if="!$store.state.isSigned" class="btn-get-started scrollto">둘러보기</button>
                 </div>
                 </div>
 
@@ -22,51 +23,65 @@
             <div class="container" data-aos="fade-up">
                 <header class="section-header">
                 <h3>Services</h3>
-                <p>인공지능 기술을 활용한 출입통제 키오스크 어플리케이션</p>
+                <hr>
+                <p></p>
                 </header>
 
                 <div class="row">
 
-                <div class="col-md-6 col-lg-4 wow bounceInUp" data-aos="zoom-in" data-aos-delay="100">
+                <div class="col-md-6 col-lg-3 wow bounceInUp" data-aos="zoom-in" data-aos-delay="100">
                     <div class="box">
-                    <div class="icon" style="background: #fceef3;"><i class="ion-ios-analytics-outline" style="color: #ff689b;"></i></div>
-                    <h4 class="title"><a href="" style="pointer-events: none; ">얼굴 인식</a></h4>
-                    <p class="description" style="text-align: center;">기존 지문 등록이 가지는 단점을 보완하여 안면 인식을 통해 출입 통제</p>
+                    <div class="icon"><img src="../../public/images/d1.png" alt=""></div>
+                    <h4 class="title"><a href="" style="pointer-events: none; ">비접촉 인터페이스</a></h4>
+                    <p class="description" style="text-align: center;">교차감염으로부터의 안전성 제고</p>
                     </div>
                 </div>
-                <div class="col-md-6 col-lg-4" data-aos="zoom-in" data-aos-delay="200">
+                <div class="col-md-6 col-lg-3" data-aos="zoom-in" data-aos-delay="200">
                     <div class="box">
-                    <div class="icon" style="background: #fff0da;"><i class="ion-ios-bookmarks-outline" style="color: #e98e06;"></i></div>
-                    <h4 class="title"><a href="" style="pointer-events: none;">음성 인식</a></h4>
-                    <p class="description" style="text-align: center;">방문객일 경우, 음성 인식을 통해 호출할 관계자를 말하면 해당 관계자에게 호출 메세지 전송</p>
+                    <div class="icon"><img src="../../public/images/d2.png" alt=""></div>
+                    <h4 class="title"><a href="" style="pointer-events: none;">모바일 키오스크</a></h4>
+                    <p class="description" style="text-align: center;">간편하고 합리적인 솔루션</p>
                     </div>
                 </div>
 
-                <div class="col-md-6 col-lg-4" data-aos="zoom-in" data-aos-delay="300">
+                <div class="col-md-6 col-lg-3" data-aos="zoom-in" data-aos-delay="300">
                     <div class="box">
-                    <div class="icon" style="background: #e6fdfc;"><i class="ion-ios-paper-outline" style="color: #3fcdc7;"></i></div>
-                    <h4 class="title"><a href="" style="pointer-events: none;">출입 로그</a></h4>
-                    <p class="description" style="text-align: center;">출입 기록을 로그로 관리하여 권한에 따라 등록된 지점의 로그 조회</p>
+                    <div class="icon"><img src="../../public/images/d3.png" alt=""></div>
+                    <h4 class="title"><a href="" style="pointer-events: none;">웹 UI</a></h4>
+                    <p class="description" style="text-align: center;">어디서든 쉽게 기록 조회 및 관리</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3" data-aos="zoom-in" data-aos-delay="300">
+                    <div class="box">
+                    <div class="icon"><img src="../../public/images/d4.png" alt=""></div>
+                    <h4 class="title"><a href="" style="pointer-events: none;">알림 연동</a></h4>
+                    <p class="description" style="text-align: center;">사내메신저와 원활한 연동</p>
                     </div>
                 </div>
             </div>
 
             </div>
         </section><!-- End Services Section -->
-          <section id="features" class="features" >
+        <section id="features" class="features" >
             <div class="container" data-aos="fade-up">
+                <header class="section-header">
+                    <h3>Features</h3>
+                    <hr>
+                    <p></p>
+                </header>
 
                 <div class="row feature-item">
                 <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
                     <img src="../../public/images/Face-recognition-1.jpg" class="images-fluid" alt="">
                 </div>
                 <div class="col-lg-6 wow fadeInUp pt-5 pt-lg-0" data-aos="fade-left" data-aos-delay="150">
-                    <h4 style="margin-top:100px;">얼굴 인식을 통해 건물 및 사무실 출입</h4>
-                    <p>
-                    Ipsum in aspernatur ut possimus sint. Quia omnis est occaecati possimus ea. Quas molestiae perspiciatis occaecati qui rerum. Deleniti quod porro sed quisquam saepe. Numquam mollitia recusandae non ad at et a.
+                    <h4 style="margin-top:100px;">얼굴 인식 <span>Face Recognition</span></h4>
+                    <p style="margin-top: 40px">
+                    이미지를 512차원의 Vector로 변환한 후, 값을 대조하여 얼굴을 인증합니다.
                     </p>
                     <p>
-                    Ad vitae recusandae odit possimus. Quaerat cum ipsum corrupti. Odit qui asperiores ea corporis deserunt veritatis quidem expedita perferendis. Qui rerum eligendi ex doloribus quia sit. Porro rerum eum eum.
+                    신속하고 정확한 본인 인증을 통해 출입 기록을 남기고, 사무실에 출입할 수 있습니다.
                     </p>
                 </div>
                 </div>
@@ -77,15 +92,12 @@
                 </div>
 
                 <div class="col-lg-6 wow fadeInUp pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-right" data-aos-delay="150">
-                    <h4 style="margin-top:80px;">음성 인식을 통해 관계자 호출</h4>
-                    <p>
-                    Delectus alias ut incidunt delectus nam placeat in consequatur. Sed cupiditate quia ea quis. Voluptas nemo qui aut distinctio. Cumque fugit earum est quam officiis numquam. Ducimus corporis autem at blanditiis beatae incidunt sunt.
+                    <h4 style="margin-top:80px;">음성 인식 <span>Voice Recognition</span></h4>
+                    <p style="margin-top: 40px">
+                    목소리를 인지하여 별도의 터치스크린을 통한 조작 없이
                     </p>
                     <p>
-                    Voluptas saepe natus quidem blanditiis. Non sunt impedit voluptas mollitia beatae. Qui esse molestias. Laudantium libero nisi vitae debitis. Dolorem cupiditate est perferendis iusto.
-                    </p>
-                    <p>
-                    Eum quia in. Magni quas ipsum a. Quis ex voluptatem inventore sint quia modi. Numquam est aut fuga mollitia exercitationem nam accusantium provident quia.
+                    간편하게 찾는 직원에게 호출 메세지를 전송합니다.
                     </p>
                 </div>
 
@@ -93,16 +105,30 @@
 
             </div>
         </section><!-- End Features Section -->
-          <!-- ======= Team Section ======= -->
-            <section id="team" class="team section-bg">
+        <!-- ======= UCC Section ======= -->
+        <section id="team" class="team section-bg">
             <div class="container" data-aos="fade-up">
                 <div class="section-header">
-                <h3>Team</h3>
-                <p>성장하기 위해 끊임없이 노력하는 개발팀 띠로링입니다. 협업을 통해 서로를 발전시킬 수 있는 팀이 되는것이 목표입니다.</p>
+                    <h3>How It Works</h3>
+                    <hr id="how-it-works" class="how-it-works">
+                    <p></p>
+                </div>
+                <div class="row">
+                    <iframe style="margin:auto; height: 315px;" width="560" height="315" src="https://www.youtube.com/embed/vmDDOFXSgAs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </section><!-- End UCC Section -->
+        <!-- ======= Team Section ======= -->
+        <section id="team" class="team">
+            <div class="container" data-aos="fade-up">
+                <div class="section-header">
+                <h3>About Us</h3>
+                <hr>
+                <h4 style="text-align:center">상호보완하며 시너지를 창출하는 개발팀 띠로링입니다.</h4>
                 </div>
 
                 <div class="row">
-                <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                <div class="col-lg-4 col-md-6 profile" data-aos="fade-up" data-aos-delay="100">
                     <div class="member">
                     <img src="../../public/images/team-1.jpg" class="images-fluid" alt="">
                     <div class="member-info">
@@ -114,7 +140,7 @@
                     </div>
                 </div>
 
-                <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+                <div class="col-lg-4 col-md-6 profile" data-aos="fade-up" data-aos-delay="200">
                     <div class="member">
                     <img src="../../public/images/team-2.jpg" class="images-fluid" alt="">
                     <div class="member-info">
@@ -126,7 +152,7 @@
                     </div>
                 </div>
 
-                <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+                <div class="col-lg-4 col-md-6 profile" data-aos="fade-up" data-aos-delay="300">
                     <div class="member">
                     <img src="../../public/images/team-3.jpg" class="images-fluid" alt="">
                     <div class="member-info">
@@ -148,7 +174,7 @@
                 &copy; Copyright <strong>WHO AM I</strong>. All Rights Reserved
             </div>
             <div class="credits">
-                Designed by <a href="https://bootstrapmade.com/">tiroring</a>
+                Designed by Tiroring
             </div>
             </div>
         </footer><!-- End  Footer -->
@@ -206,6 +232,10 @@ export default {
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap');
+
+div {
+    color: #707070;
+}
 
 .banner{
     height:500px;
