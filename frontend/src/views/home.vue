7b5696227452db1c70989669b85df743bcf34816
tiroring09 <tiroring09@gmail.com>
2020-11-17 14:41:35 +0900

    Update UCC동영상 업로드

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -114,7 +114,7 @@
                     <p></p>
                 </div>
                 <div class="row">
-                    <iframe style="margin:auto; height: 315px;" width="560" height="315" src="https://www.youtube.com/embed/vmDDOFXSgAs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe style="margin:auto; height: 315px;" width="560" height="315" src="https://www.youtube.com/embed/hzqgPfFtRno" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
                 <div>
                     <img id="web_pages" src="../../public/images/web_pages.gif" class="images-fluid" alt="">
